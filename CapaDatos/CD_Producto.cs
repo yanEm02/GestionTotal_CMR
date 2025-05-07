42250d2708258b8a3408fb0d57f649e2f6763fe0
Yan S <yanreyes654@gmail.com>
2025-05-07 11:58:50 -0400

    feat: ventana producto y sus funciones

---
+++ CapaDatos/CD_Producto.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaEntidad;
+using System.Collections;
+using System.Reflection;
+
+
+namespace CapaDatos
+{
+    public class CD_Producto
+    {
+        public List<Producto> Listar()
+        {
+            List<Producto> lista = new List<Producto>();
+
+            //conexion a BD
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                //capturador de errores en caso de algun problema con la base de datos
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("select ID_PRODUCTO, CODIGO, NOMBRE, p.DESCRIPCION,c.ID_CATEGORIA,c.DESCRIPCION        [DescripcionCategoria],STOCK,PRECIO_COMPRA,PRECIO_VENTA, p.Estado from PRODUCTO p");
+                    query.AppendLine("inner join CATEGORIA c on c.ID_CATEGORIA = p.CATEGORIA");
+
+                    SqlCommand cmd = new SqlCommand(query.ToString(), oconexion);
+                    cmd.CommandType = CommandType.Text; //para declararr el tipo de comando ya que es una consulta con select
+
+                    oconexion.Open();//abrir cadena de conexion
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        //mientras comando lea desde la cadena de base de datos entonces va a ir almacenando los datos 
+                        while (dr.Read())
+                        {
+                            lista.Add(new Producto()
+                            {
+                                IdProducto = Convert.ToInt32(dr["Id_Producto"]),
+                                Codigo = dr["Codigo"].ToString(),
+                                Nombre = dr["NOMBRE"].ToString(),
+                                Descripcion = dr["DESCRIPCION"].ToString(),
+                                oCategoria = new Categoria() { IdCategoria = Convert.ToInt32(dr["ID_CATEGORIA"]),
+                                Descripcion = dr["DescripcionCategoria"].ToString() },
+                                Stock = Convert.ToInt32(dr["STOCK"].ToString()),
+                                PrecioCompra = Convert.ToDecimal(dr["PRECIO_COMPRA"].ToString()),
+                                PrecioVenta = Convert.ToDecimal(dr["PRECIO_VENTA"].ToString()),
+                                Estado = dr["Estado"] != DBNull.Value ? Convert.ToBoolean(dr["Estado"]) : false,
+                                //Estado = Convert.ToBoolean(dr["Estado"]),
+                            });
+
+                        }
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    lista = new List<Producto>();
+                }
+            }
+
+            return lista;
+        }
+
+        //creamos el metodo para conectar a la base de datos con el proc almacenado REGISTRAR Producto
+        public int Registrar(Producto obj, out string Mensaje)
+        {
+            int idProductoGenerado = 0;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_REGISTRAR_Producto", oconexion);
+                    cmd.Parameters.AddWithValue("Codigo", obj.Codigo);
+                    cmd.Parameters.AddWithValue("Nombre", obj.Nombre);
+                    cmd.Parameters.AddWithValue("Descripcion", obj.Descripcion);
+                    cmd.Parameters.AddWithValue("IdCategoria", obj.oCategoria.IdCategoria);
+                    cmd.Parameters.AddWithValue("Estado", obj.Estado);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure; //para declararr el tipo de comando ya que es un proc almacenado
+
+                    oconexion.Open();//abrir cadena de conexion
+
+                    cmd.ExecuteNonQuery();
+
+                    idProductoGenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+
+            }
+            catch (Exception e)
+            {
+                idProductoGenerado = 0;
+                Mensaje = e.Message;
+            }
+
+
+            return idProductoGenerado;
+        }
+
+        //creamos el metodo para conectar a la base de datos con el proc almacenado EDITAR
+        public bool Editar(Producto obj, out string Mensaje)
+        {
+            bool respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_EDITAR_PRODUCTO", oconexion);
+                    cmd.Parameters.AddWithValue("IdProducto", obj.IdProducto);
+                    cmd.Parameters.AddWithValue("Codigo", obj.Codigo);
+                    cmd.Parameters.AddWithValue("Nombre", obj.Nombre);
+                    cmd.Parameters.AddWithValue("Descripcion", obj.Descripcion);
+                    cmd.Parameters.AddWithValue("IdCategoria", obj.oCategoria.IdCategoria);
+                    cmd.Parameters.AddWithValue("Estado", obj.Estado);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure; //para declararr el tipo de comando ya que es un proc almacenado
+
+                    oconexion.Open();//abrir cadena de conexion
+
+                    cmd.ExecuteNonQuery(); //ejecutamos el prco
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+
+            }
+            catch (Exception e)
+            {
+                respuesta = false;
+                Mensaje = e.Message;
+            }
+            return respuesta;
+        }
+
+        //creamos el metodo para conectar a la base de datos con el proc almacenado EELIMINAR 
+        public bool Eliminar(Producto obj, out string Mensaje)
+        {
+            bool respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_ELIMINAR_Producto", oconexion);
+                    cmd.Parameters.AddWithValue("IdProducto", obj.IdProducto);
+                    cmd.Parameters.Add("Respuesta", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure; //para declararr el tipo de comando ya que es un proc almacenado
+
+                    oconexion.Open();//abrir cadena de conexion
+
+                    cmd.ExecuteNonQuery(); //ejecutamos el prco
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Respuesta"].Value);
+                    Mensaje = cmd.Parameters["mensaje"].Value.ToString();
+                }
+
+            }
+            catch (Exception e)
+            {
+                respuesta = false;
+                Mensaje = e.Message;
+            }
+
+
+            return respuesta;
+        }
+
+
+
+    }
+}
--- CapaPresentacion/frmProducto.cs
+++ CapaPresentacion/frmProducto.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CapaEntidad;
+using CapaNegocio;
+using CapaPresentacion.Utilidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,9 +20,266 @@ namespace CapaPresentacion
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void frmProducto_Load(object sender, EventArgs e)
         {
+            //COMBOBOX DE ESTADO
+            //agregamos los items del combobox para desplegarlos, usando la clase dentro de utilidades, usamos clases y objetos 
+            cboEstado.Items.Add(new OpcionCombo() { Valor = 1, Texto = "Activo" });
+            cboEstado.Items.Add(new OpcionCombo() { Valor = 0, Texto = "No Activo" });
+            cboEstado.DisplayMember = "Texto";
+            cboEstado.ValueMember = "Valor";
+            cboEstado.SelectedIndex = 0;
 
+            //hacemos una lista para traer los roles de la base de datos y listarlos con un foreach
+            List<Categoria> listaCategoria = new CN_Categoria().Listar();
+
+            foreach (Categoria item in listaCategoria)
+            {
+                cboCategoria.Items.Add(new OpcionCombo() { Valor = item.IdCategoria, Texto = item.Descripcion });
+            }
+            cboCategoria.DisplayMember = "Texto";
+            cboCategoria.ValueMember = "Valor";
+            cboCategoria.SelectedIndex = 0;
+
+
+            //[para realizar el filtro por columna 
+            foreach (DataGridViewColumn columna in dgvData.Columns)
+            {
+                if (columna.Visible == true && columna.Name != "btnSeleccionar")
+                {
+                    cboBusqueda.Items.Add(new OpcionCombo() { Valor = columna.Name, Texto = columna.HeaderText });
+                }
+            }
+            cboBusqueda.DisplayMember = "Texto";
+            cboBusqueda.ValueMember = "Valor";
+            cboBusqueda.SelectedIndex = 0;
+
+            //mostrar todos los usuarios en el data grid view
+            List<Producto> lista = new CN_Producto().Listar();
+
+            foreach (Producto item in lista)
+            {
+                dgvData.Rows.Add(new object[] { "", 
+                    item.IdProducto,
+                    item.Codigo,
+                    item.Nombre,
+                    item.Descripcion,
+                    item.oCategoria.IdCategoria,
+                    item.oCategoria.Descripcion,
+                    item.Stock,
+                    item.PrecioCompra,
+                    item.PrecioVenta,
+                    item.Estado == true ? 1 : 0,
+                    item.Estado == true ? "Activo" : "No Activo",
+                });
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string mensaje = string.Empty;
+
+            Producto obj = new Producto()
+            {
+                IdProducto = Convert.ToInt32(txtid.Text),
+                Codigo = txtCodigo.Text,
+                Nombre = txtNombre.Text,
+                Descripcion = txtDescripcion.Text,
+                oCategoria = new Categoria() { IdCategoria = Convert.ToInt32(((OpcionCombo)cboCategoria.SelectedItem).Valor) },
+                Estado = Convert.ToInt32(((OpcionCombo)cboEstado.SelectedItem).Valor) == 1 ? true : false,
+            };
+
+            //hacemos una condicional para decidir el comportamiento del boton guardar, si es para editar si hay un usuario selccionado o guardar si no hay usuario seleccionado 
+            if (obj.IdProducto == 0)
+            {
+                int idGenerado = new CN_Producto().Registrar(obj, out mensaje);
+
+                if (idGenerado != 0)
+                {
+                    //aqui agremos lo que este en el textbox del formulario para agregarse a la data grid view
+                    dgvData.Rows.Add(new object[] { "",
+                        idGenerado,
+                        txtCodigo.Text,
+                        txtNombre.Text,
+                        txtDescripcion.Text,
+                        ((OpcionCombo)cboCategoria.SelectedItem).Valor.ToString(), //para obtener el estado de la base de datos
+                        ((OpcionCombo)cboCategoria.SelectedItem).Texto.ToString(),
+                        "0",
+                        "0.00",
+                        "0.00",
+                        ((OpcionCombo)cboEstado.SelectedItem).Valor.ToString(), //para obtener el estado de la base de datos
+                        ((OpcionCombo)cboEstado.SelectedItem).Texto.ToString(),
+                    });
+
+                    Limpiar();
+                }
+                else
+                {
+                    MessageBox.Show(mensaje);
+                }
+            }
+            else //aqui ejecuta el metodo editar si el indice seleccionado es el de un usuario existente 
+            {
+                bool resultado = new CN_Producto().Editar(obj, out mensaje);
+
+                if (resultado)
+                {
+                    DataGridViewRow row = dgvData.Rows[Convert.ToInt32(txtIndice.Text)];
+                    row.Cells["Id"].Value = txtid.Text;
+                    row.Cells["Codigo"].Value = txtCodigo.Text;
+                    row.Cells["Nombre"].Value = txtNombre.Text;
+                    row.Cells["Descripcion"].Value = txtDescripcion.Text;
+                    row.Cells["Id_Categoria"].Value = ((OpcionCombo)cboCategoria.SelectedItem).Valor.ToString();
+                    row.Cells["Categoria"].Value = ((OpcionCombo)cboCategoria.SelectedItem).Texto.ToString();
+                    row.Cells["EstadoValor"].Value = ((OpcionCombo)cboEstado.SelectedItem).Valor.ToString();
+                    row.Cells["Estado"].Value = ((OpcionCombo)cboEstado.SelectedItem).Texto.ToString();
+
+                    Limpiar();
+                }
+                else
+                {
+                    MessageBox.Show(mensaje);
+                }
+            }
+        }
+
+
+        //limpiamos los datos introducidos despues de guardar 
+        private void Limpiar()
+        {
+            txtIndice.Text = "-1";
+            txtid.Text = "0";
+            txtCodigo.Text = "";
+            txtNombre.Text = "";
+            txtDescripcion.Text = "";
+            cboCategoria.SelectedIndex = 0;
+            cboEstado.SelectedIndex = 0;
+
+            txtCodigo.Select();
+        }
+
+        private void dgvData_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            if (e.RowIndex < 0)
+            {
+                return;
+            }
+            if (e.ColumnIndex == 0)
+            {
+                e.Paint(e.CellBounds, DataGridViewPaintParts.All);
+
+                var w = Properties.Resources.check_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.Width;
+                var h = Properties.Resources.check_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.Height;
+                var x = e.CellBounds.Left + (e.CellBounds.Width - w) / 2;
+                var y = e.CellBounds.Top + (e.CellBounds.Height - h) / 2;
+
+                e.Graphics.DrawImage(Properties.Resources.check_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24, new Rectangle(x, y, w, h));
+                e.Handled = true;
+            }
+        }
+
+        private void dgvData_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvData.Columns[e.ColumnIndex].Name == "btnSeleccionar")
+            {
+                int indice = e.RowIndex;
+
+                if (indice >= 0)
+                {
+                    txtIndice.Text = indice.ToString();
+                    txtid.Text = dgvData.Rows[indice].Cells["Id"].Value.ToString();
+
+                    txtCodigo.Text = dgvData.Rows[indice].Cells["Codigo"].Value.ToString();
+                    txtNombre.Text = dgvData.Rows[indice].Cells["Nombre"].Value.ToString();
+                    txtDescripcion.Text = dgvData.Rows[indice].Cells["Descripcion"].Value.ToString();
+
+                    //agreagamos los combobox
+                    foreach (OpcionCombo oc in cboCategoria.Items)
+                    {
+                        if (Convert.ToInt32(oc.Valor) == Convert.ToInt32(dgvData.Rows[indice].Cells["Id_Categoria"].Value))
+                        {
+                            int indice_combo = cboCategoria.Items.IndexOf(oc);
+                            cboCategoria.SelectedIndex = indice_combo;
+                            break;
+                        }
+                    }
+
+                    foreach (OpcionCombo oc in cboEstado.Items)
+                    {
+                        if (Convert.ToInt32(oc.Valor) == Convert.ToInt32(dgvData.Rows[indice].Cells["EstadoValor"].Value))
+                        {
+                            int indice_combo = cboEstado.Items.IndexOf(oc);
+                            cboEstado.SelectedIndex = indice_combo;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(txtid.Text) != 0)
+            {
+                if (MessageBox.Show("Desea Eliminar el Producto?", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+
+                    //sacamos el id usuario de nuestra clase de usuario 
+                    string mensaje = string.Empty;
+                    Producto obj = new Producto()
+                    {
+                        IdProducto = Convert.ToInt32(txtid.Text),
+                    };
+                    bool respuesta = new CN_Producto().Eliminar(obj, out mensaje);
+
+                    if (respuesta)
+                    {
+                        dgvData.Rows.RemoveAt(Convert.ToInt32(txtIndice.Text));
+                        Limpiar();
+                    }
+                    else
+                    {
+                        MessageBox.Show(mensaje, "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+
+            }
+            Limpiar();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string columnaFiltro = ((OpcionCombo)cboBusqueda.SelectedItem).Valor.ToString();
+
+            if (dgvData.Rows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dgvData.Rows)
+                {
+                    //hacemos el filtro con un foreach, limpiando los espacios y conviertiendo a mayus
+                    if (row.Cells[columnaFiltro].Value.ToString().Trim().ToUpper().Contains(txtBusqueda.Text.Trim().ToUpper()))
+                    {
+                        row.Visible = true;
+                    }
+                    else
+                    {
+                        row.Visible = false;
+                    }
+                }
+            }
+        }
+
+        private void btnLimpiarBuscador_Click(object sender, EventArgs e)
+        {
+            txtBusqueda.Text = "";
+            foreach (DataGridViewRow row in dgvData.Rows)
+            {
+                row.Visible = true;
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
         }
     }
 }
